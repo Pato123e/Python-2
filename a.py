64b244ead0ba5879567fe265ee8375992146af65
leumaslinkedin <samuellinkedingithub@gmail.com>
2024-07-09 20:09:38 -0300

    a

---
+++ a.py
@@ -0,0 +1,10 @@
+oi = input("english or spanish ou portugues?")
+
+if oi == "english":
+    print("anyone who moves is gay")
+
+elif oi == "spanish":
+    print("cualquiera que se mueva es gay")
+
+else:
+    print("quem se mexer primeiro e viado")
